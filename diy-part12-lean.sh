91e6616ab5b02b43ca53189e4e587733b4c572a9
Aceslup <aceslup@users.noreply.github.com>
2023-04-01 03:42:28 +0800

    Update diy-part12-lean.sh

--- diy-part12-lean.sh
+++ diy-part12-lean.sh
@@ -10,17 +10,21 @@ tree ./feeds/packages/utils/syncthing
 # aliyundrive-webdav
 rm -rf ./feeds/packages/multimedia/aliyundrive-webdav
 cp -r ./feeds/smpackage/aliyundrive-webdav ./feeds/packages/multimedia/
+#rm -rf ./feeds/packages/applications/luci-app-aliyundrive-webdav
+#cp -r ./feeds/smpackage/luci-app-aliyundrive-webdav ./feeds/packages/applications/
 
 # aliyundrive-fuse
 rm -rf ./feeds/packages/multimedia/aliyundrive-fuse
 cp -r ./feeds/smpackage/aliyundrive-fuse ./feeds/packages/multimedia/
+#rm -rf ./feeds/packages/applications/luci-app-aliyundrive-fuse
+#cp -r ./feeds/smpackage/luci-app-aliyundrive-fuse ./feeds/packages/applications/
 
 # UnblockNeteaseMusic-Go
-rm -rf ./feeds/packages/multimedia/UnblockNeteaseMusic-Go
-cp -r ./feeds/smpackage/UnblockNeteaseMusic-Go ./feeds/packages/multimedia/
+#rm -rf ./feeds/packages/multimedia/UnblockNeteaseMusic-Go
+#cp -r ./feeds/smpackage/UnblockNeteaseMusic-Go ./feeds/packages/multimedia/
 
 # UnblockNeteaseMusic
 rm -rf ./feeds/packages/multimedia/UnblockNeteaseMusic
-cp -r ./feeds/smpackage/UnblockNeteaseMusic ./feeds/packages/multimedia/
+#cp -r ./feeds/smpackage/UnblockNeteaseMusic ./feeds/packages/multimedia/
 
 ./scripts/feeds update -a
