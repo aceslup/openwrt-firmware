88f6c07bc3fb469ebb136d3e8f497981cdc6a280
Aceslup <aceslup@users.noreply.github.com>
2023-03-31 18:27:52 +0800

    Create diy-part12-lean.sh
    feeds update 之后清理一些软件包

---
+++ diy-part12-lean.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+
+# 清理 coolsnowwolf/packages 无需的软件包。改由 kenzok8/small-package 提供
+# syncthing
+rm -rf ./feeds/packages/utils/syncthing/files/etc/config/syncthing
+rm -rf ./feeds/packages/utils/syncthing/files/etc/init.d/syncthing
+tree ./feeds/packages/utils/syncthing 
+
+# aliyundrive-webdav
+rm -rf ./feeds/packages/multimedia/aliyundrive-webdav
+
+# aliyundrive-fuse
+rm -rf ./feeds/packages/multimedia/aliyundrive-fuse
+
+# UnblockNeteaseMusic-Go
+rm -rf ./feeds/packages/multimedia/UnblockNeteaseMusic-Go
+
+# UnblockNeteaseMusic
+rm -rf ./feeds/packages/multimedia/UnblockNeteaseMusic
