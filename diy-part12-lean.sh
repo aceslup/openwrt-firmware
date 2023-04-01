ec7ca623c6477dea1bc255745c69f39d6a8994b7
Aceslup <aceslup@users.noreply.github.com>
2023-04-01 16:53:41 +0800

    Update diy-part12-lean.sh

--- diy-part12-lean.sh
+++ diy-part12-lean.sh
@@ -3,9 +3,9 @@
 
 # 清理 coolsnowwolf/packages 无需的软件包。改由 kenzok8/small-package 提供
 # syncthing
-rm -rf ./feeds/packages/utils/syncthing/files/etc/config/syncthing
-rm -rf ./feeds/packages/utils/syncthing/files/etc/init.d/syncthing
-tree ./feeds/packages/utils/syncthing 
+#rm -rf ./feeds/packages/utils/syncthing/files/etc/config/syncthing
+#rm -rf ./feeds/packages/utils/syncthing/files/etc/init.d/syncthing
+#tree ./feeds/packages/utils/syncthing 
 
 # aliyundrive-webdav
 rm -rf ./feeds/packages/multimedia/aliyundrive-webdav
