630df4c31145de3c1d89300038b1125375e746d1
Aceslup <aceslup@users.noreply.github.com>
2023-09-14 09:27:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,11 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+
+# 解决rust问题
+curl -o ./feeds/packages/lang/rust/rust-values.mk \
+  https://raw.githubusercontent.com/Jason6111/packages/patch-1/lang/rust/rust-values.mk
+curl -o ./feeds/packages/lang/rust/Makefile \
+  https://raw.githubusercontent.com/Jason6111/OpenWrt_Personal/main/other/rust/Makefile
+curl -o ./feeds/packages/lang/rust/patches/0001-Update-xz2-and-use-it-static.patch \
+  https://raw.githubusercontent.com/Jason6111/OpenWrt_Personal/main/other/rust/0001-Update-xz2-and-use-it-static.patch
