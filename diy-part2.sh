237e40e670f0ae7d157e2a8cf3ed936736fcd4d2
Aceslup <aceslup@users.noreply.github.com>
2023-09-14 15:02:28 +0800

    Update diy-part2.sh
    rust，cargo，ring

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
+# Rust & ring
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+apt install cargo -y
+cargo install ring
+
 # Upgrage Rust: 1.68.1 to 1.72.0
 curl -o ./feeds/packages/lang/rust/rust-values.mk \
   https://raw.githubusercontent.com/Jason6111/packages/patch-1/lang/rust/rust-values.mk
