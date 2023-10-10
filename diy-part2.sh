8e5b936660d50ae389254ba203661814b81051f0
Aceslup <aceslup@users.noreply.github.com>
2023-10-11 01:32:22 +0800

    Update diy-part2.sh
    do not upgrade rust

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,14 +12,14 @@
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # Rust & ring
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-apt install cargo -y
-cargo install ring
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#apt install cargo -y
+#cargo install ring
 
 # Upgrage Rust: 1.68.1 to 1.72.0
-curl -o ./feeds/packages/lang/rust/rust-values.mk \
-  https://raw.githubusercontent.com/Jason6111/packages/patch-1/lang/rust/rust-values.mk
-curl -o ./feeds/packages/lang/rust/Makefile \
-  https://raw.githubusercontent.com/Jason6111/OpenWrt_Personal/main/other/rust/Makefile
-curl -o ./feeds/packages/lang/rust/patches/0001-Update-xz2-and-use-it-static.patch \
-  https://raw.githubusercontent.com/Jason6111/OpenWrt_Personal/main/other/rust/0001-Update-xz2-and-use-it-static.patch
+#curl -o ./feeds/packages/lang/rust/rust-values.mk \
+#  https://raw.githubusercontent.com/Jason6111/packages/patch-1/lang/rust/rust-values.mk
+#curl -o ./feeds/packages/lang/rust/Makefile \
+#  https://raw.githubusercontent.com/Jason6111/OpenWrt_Personal/main/other/rust/Makefile
+#curl -o ./feeds/packages/lang/rust/patches/0001-Update-xz2-and-use-it-static.patch \
+#  https://raw.githubusercontent.com/Jason6111/OpenWrt_Personal/main/other/rust/0001-Update-xz2-and-use-it-static.patch
