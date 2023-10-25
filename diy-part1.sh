ada42eacbfd820e38afda100aae5b91e7644a7c6
Aceslup <aceslup@users.noreply.github.com>
2023-10-25 14:26:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -37,6 +37,24 @@ sed -e 's/ddns-scripts_aliyun//g' \
 #【lean】：禁用 coolsnowwolf/luci/applications 软件源
 #sed -i 's/\(^src-git luci.*\)/#\1/' feeds.conf.default
 
+# 清理 coolsnowwolf/packages 无需的软件包。改由 kenzok8/small-package 提供
+# syncthing
+#rm -rf ./feeds/packages/utils/syncthing/files/etc/config/syncthing
+#rm -rf ./feeds/packages/utils/syncthing/files/etc/init.d/syncthing
+#tree ./feeds/packages/utils/syncthing 
+
+# aliyundrive-webdav
+rm -rf ./feeds/packages/multimedia/aliyundrive-webdav
+#cp -r ./feeds/smpackage/aliyundrive-webdav ./feeds/packages/multimedia/
+rm -rf ./feeds/luci/applications/luci-app-aliyundrive-webdav
+#cp -r ./feeds/smpackage/luci-app-aliyundrive-webdav ./feeds/luci/applications/
+
+# aliyundrive-fuse
+rm -rf ./feeds/packages/multimedia/aliyundrive-fuse
+#cp -r ./feeds/smpackage/aliyundrive-fuse ./feeds/packages/multimedia/
+rm -rf ./feeds/luci/applications/luci-app-aliyundrive-fuse
+#cp -r ./feeds/smpackage/luci-app-aliyundrive-fuse ./feeds/luci/applications/
+
 
 #【lienol】: passwall,syncthing
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
