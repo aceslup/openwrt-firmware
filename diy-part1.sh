c9218d08abff275aebd75809139ce2d89edcc1ad
Aceslup <aceslup@users.noreply.github.com>
2024-04-27 22:02:26 +0800

    优先载入kenzok8/small-package包

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,7 +23,7 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # 追新软件包
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 
 #【tty228】: luci-app-wechatpush
