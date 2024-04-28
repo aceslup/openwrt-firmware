470f35015966981f30a56c43b77fae3e3f8b420c
Aceslup <aceslup@users.noreply.github.com>
2024-04-28 22:25:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,6 +8,8 @@
 # Blog: https://p3terx.com
 #=============================================================
 
+# PWD == openwrt
+
 #【lienol】: 去掉无刚需的默认插件: ./include/target.mk
 sed -e 's/luci-app-ramfree//g' \
     -e 's/luci-app-ddns//g' \
@@ -16,6 +18,46 @@ sed -e 's/luci-app-ramfree//g' \
     -e 's/ddns-scripts_dnspod//g' \
     -e 's/luci-app-timecontrol//g' \
     -i ./include/target.mk
+
+
+# 重写插件源
+#tee ./feeds.conf.default <<-'EOF'
+#
+## kenzok8: 追新插件包
+#src-git smpackage https://github.com/kenzok8/small-package
+#
+## 官方依赖包
+#src-git routing https://git.openwrt.org/feed/routing.git
+#src-git telephony https://git.openwrt.org/feed/telephony.git
+#
+## lienol: 依赖组件包
+#src-git packages https://github.com/Lienol/openwrt-packages.git;master
+#src-git luci https://github.com/Lienol/openwrt-luci.git;master
+#
+## lienol: 插件包
+##src-git lienol https://github.com/Lienol/openwrt-package.git;main
+##src-git other https://github.com/Lienol/openwrt-package.git;other
+## xiaorouji: passwall
+##src-git passwall https://github.com/xiaorouji/openwrt-passwall
+#
+## lean: ssr+
+##src-git helloworld https://github.com/fw876/helloworld
+##src-git lean https://github.com/coolsnowwolf/packages
+#
+## tty228: wechatpush
+#src-git wechatpush https://github.com/tty228/luci-app-wechatpush
+#
+## homelede: homeconnect,ssr+,smartdns,serverchan,jd-dailybonus
+##src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng
+#
+## vernesong: openclash
+##src-git OpenClash https://github.com/vernesong/OpenClash
+#
+## nxhack: node
+##src-git node https://github.com/nxhack/openwrt-node-packages.git
+#
+#EOF
+
 
 #【lienol】: passwall,syncthing
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
@@ -37,10 +79,6 @@ sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.con
 
 #【tty228】: luci-app-wechatpush
 git clone https://github.com/tty228/luci-app-wechatpush `pwd`/package/luci-app-wechatpush
-
-
-#【xiaorouji】:
-#sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
 
 #【homelede】: homeconnect,ssr+,smartdns,serverchan,jd-dailybonus
