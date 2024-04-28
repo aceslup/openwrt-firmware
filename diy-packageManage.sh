c219ca4e9bf803f0f5d94974aec294361202cf89
Aceslup <aceslup@users.noreply.github.com>
2024-04-28 15:36:37 +0800

    Update diy-packageManage.sh

--- diy-packageManage.sh
+++ diy-packageManage.sh
@@ -4,12 +4,15 @@
 
 ## Go Version = 1.22.2
 ## https://golang.google.cn/dl/
-sed -e 's/\(GO_VERSION_MAJOR_MINOR:\)=.*/\1=1.22/' \
-    -e 's/\(GO_VERSION_PATCH:\)=.*/\1=2/' \
-    -e 's/\(PKG_HASH:\)=.*/\1=58f0c5ced45a0012bce2ff7a9df03e128abcc8818ebabe5027bb92bafe20e421/' \
-    -i ./feeds/packages/lang/golang/golang/Makefile
+#sed -e 's/\(GO_VERSION_MAJOR_MINOR:\)=.*/\1=1.22/' \
+#    -e 's/\(GO_VERSION_PATCH:\)=.*/\1=2/' \
+#    -e 's/\(PKG_HASH:\)=.*/\1=58f0c5ced45a0012bce2ff7a9df03e128abcc8818ebabe5027bb92bafe20e421/' \
+#    -i ./feeds/packages/lang/golang/golang/Makefile
 
 ## Rust Version = stable
 ### Optional: stable, beta, nightly, nightly-2018-08-01, 1.30.0
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup install stable
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo rustup install stable
+
+#sudo wget https://sh.rustup.rs -LO rustup-init.sh \
+#  && sudo sh ./rustup-init.sh -y
