507cb95dd45d60120fd4346313cc31ec6491f9d4
Aceslup <aceslup@users.noreply.github.com>
2024-04-28 19:04:16 +0800

    Update diy-packageManage.sh
    直接修改的总是报错, 尝试替换比较激进的 coolsnowwolf/packages/lang/golang 目录

--- diy-packageManage.sh
+++ diy-packageManage.sh
@@ -4,10 +4,17 @@
 
 ## Go Version = 1.22.2
 ## https://golang.google.cn/dl/
-sed -e 's/\(GO_VERSION_MAJOR_MINOR:\)=.*/\1=1.22/' \
-    -e 's/\(GO_VERSION_PATCH:\)=.*/\1=2/' \
-    -e 's/\(PKG_HASH:\)=.*/\1=374ea82b289ec738e968267cac59c7d5ff180f9492250254784b2044e90df5a9/' \
-    -i ./feeds/packages/lang/golang/golang/Makefile
+#sed -e 's/\(GO_VERSION_MAJOR_MINOR:\)=.*/\1=1.22/' \
+#    -e 's/\(GO_VERSION_PATCH:\)=.*/\1=2/' \
+#    -e 's/\(PKG_HASH:\)=.*/\1=374ea82b289ec738e968267cac59c7d5ff180f9492250254784b2044e90df5a9/' \
+#    -i ./feeds/packages/lang/golang/golang/Makefile
+
+## 直接修改的总是报错, 尝试替换比较激进的 coolsnowwolf/packages/lang/golang 目录
+rm -rf ./feeds/packages/lang/golang
+git clone https://github.com/coolsnowwolf/packages lean_packages
+mv ./lean_packages/lang/golang ./feeds/packages/lang/
+rm -rf ./lean_packages
+
 
 ## Rust Version = stable
 ### Optional: stable, beta, nightly, nightly-2018-08-01, 1.30.0
