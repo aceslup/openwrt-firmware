c90079472886468ca3d928843eabfd316ff8fa19
Aceslup <aceslup@users.noreply.github.com>
2024-04-28 12:51:49 +0800

    Update diy-packageManage.sh
    不同版本对应不同的PKG_HASH值

--- diy-packageManage.sh
+++ diy-packageManage.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #
-
 # PWD == openwrt
+
 ## Go Version = 1.22.2
-sed -e 's/\(GO_VERSION_MAJOR_MINOR:=\).*/\11.22/' \
-    -e 's/\(GO_VERSION_PATCH:=\).*/\12/' \
+## https://golang.google.cn/dl/
+sed -e 's/\(GO_VERSION_MAJOR_MINOR\).*/\1:=1.22/' \
+    -e 's/\(GO_VERSION_PATCH\).*/\1:=2/' \
+    -e 's/\(PKG_HASH\).*/\1:=58f0c5ced45a0012bce2ff7a9df03e128abcc8818ebabe5027bb92bafe20e421/' \
     -i ./feeds/packages/lang/golang/golang/Makefile
 
 ## Rust Version = stable
-### stable, beta, nightly, 1.30.0, nightly-2018-08-01
+### Optional: stable, beta, nightly, nightly-2018-08-01, 1.30.0
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup install stable
