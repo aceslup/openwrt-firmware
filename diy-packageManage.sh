aa57ff9dea6bdfe57a70c2598cf62ba43efaa6ff
Aceslup <aceslup@users.noreply.github.com>
2024-04-28 15:31:30 +0800

    Create diy-packageManage.sh

---
+++ diy-packageManage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# PWD == openwrt
+
+## Go Version = 1.22.2
+## https://golang.google.cn/dl/
+sed -e 's/\(GO_VERSION_MAJOR_MINOR:\)=.*/\1=1.22/' \
+    -e 's/\(GO_VERSION_PATCH:\)=.*/\1=2/' \
+    -e 's/\(PKG_HASH:\)=.*/\1=58f0c5ced45a0012bce2ff7a9df03e128abcc8818ebabe5027bb92bafe20e421/' \
+    -i ./feeds/packages/lang/golang/golang/Makefile
+
+## Rust Version = stable
+### Optional: stable, beta, nightly, nightly-2018-08-01, 1.30.0
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup install stable
