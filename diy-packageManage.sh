b2c37ea9a6e8a5217f7307bedeb1f106c44d7150
Aceslup <aceslup@users.noreply.github.com>
2024-04-29 09:02:19 +0800

    Update diy-packageManage.sh

--- diy-packageManage.sh
+++ diy-packageManage.sh
@@ -10,14 +10,14 @@
 #    -i ./feeds/packages/lang/golang/golang/Makefile
 
 ## 直接修改的总是报错, 尝试替换比较激进的 coolsnowwolf/packages/lang/golang 目录
-rm -rf ./feeds/packages/lang/golang
-git clone https://github.com/coolsnowwolf/packages lean_packages
-mv ./lean_packages/lang/golang ./feeds/packages/lang/
-rm -rf ./lean_packages
+#rm -rf ./feeds/packages/lang/golang
+#git clone https://github.com/coolsnowwolf/packages lean_packages
+#mv ./lean_packages/lang/golang ./feeds/packages/lang/
+#rm -rf ./lean_packages
 
 
 ## Rust Version = stable
 ### Optional: stable, beta, nightly, nightly-2018-08-01, 1.30.0
 ### wget https://sh.rustup.rs -LO rustup-init.sh
-sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup-init.sh \
-  && sudo sh ./rustup-init.sh -y
+#sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup-init.sh \
+#  && sudo sh ./rustup-init.sh -y
